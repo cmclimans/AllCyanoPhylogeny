b90dd1c7e99cfaaee599dc05ff7e5c0fbe82feef
cmclimans <cmclimans@ou.edu>
2024-07-24 12:27:53 -0500

    add undo function to rename

--- Simple_File_Rename.py
+++ Simple_File_Rename.py
@@ -20,45 +20,67 @@ parser.add_argument("-i", "--input", help="Input rename file key", default="rena
 parser.add_argument("-s", "--sourcename", help="Column name with existing file names", default="Assembly.file.name")
 parser.add_argument("-r", "--rename", help="Column with names to become the new name", default="Organism.Name")
 parser.add_argument("-k", "--keeporiginal", help="Boolean keep original files", default="F")
+parser.add_argument("-u", "--undo", help="Copy renamed files back to genome accession", default="F")
 
 
 args = parser.parse_args()
+key = pd.read_csv(args.input)
+
 
 if args.keeporiginal != "T" and args.keeporiginal != "F":
     exit("-k/--keeporiginal must be T or F")
     
-
-if not os.path.isdir("renamed"):
-    print("Creating directory \'renamed\' for renamed files.")
-    os.mkdir("renamed")
-
-key = pd.read_csv(args.input)
-
-if __name__ == '__main__':
     
-    problemfiles = []
-
+def undo():
     indicies = len(key)
 
     for index in range(0,indicies):
         current_name = key.iloc[index][args.sourcename]+'.fna'
         new_name = key.iloc[index][args.rename]+'.fna'
         
-        if os.path.isfile('renamed/' + new_name):
-            # print(f"{current_name} already moved.")
-            continue
-        elif not os.path.isfile('genomes/' + current_name) and not os.path.isfile('renamed/' + new_name):
-            print(f"{current_name} needs checked.")
-            problemfiles.append(current_name)
-        else:
-            if args.keeporiginal == "F":
-                shutil.copy('genomes/' + current_name, 'renamed/' + new_name)
-            else:
-                os.rename('genomes/' + current_name, 'renamed/' + new_name)
+        if os.path.isfile('renamed/' + new_name) and not os.path.isfile('genomes/' + current_name):
+            shutil.copy('renamed/' + new_name, 'genomes/' + current_name)
+
+
+if __name__ == "__main__":
+    
+    if args.undo == "T":
+        undo()
+    
+    else:
+        if not os.path.isdir("renamed"):
+            print("Creating directory \'renamed\' for renamed files.")
+            os.mkdir("renamed")
+        
+        
+        if __name__ == '__main__':
+            
+            problemfiles = []
+        
+            indicies = len(key)
+        
+            for index in range(0,indicies):
+                current_name = key.iloc[index][args.sourcename]+'.fna'
+                new_name = key.iloc[index][args.rename]+'.fna'
                 
-                
-    if len(problemfiles) > 0:
-        with open("rename_check.txt", 'w') as f:
-            for item in problemfiles:
-                f.writelines(item+'\n')
-                
+                if os.path.isfile('renamed/' + new_name):
+                    # print(f"{current_name} already moved.")
+                    continue
+                elif not os.path.isfile('genomes/' + current_name) and not os.path.isfile('renamed/' + new_name):
+                    print(f"{current_name} needs checked.")
+                    problemfiles.append(current_name)
+                else:
+                    if args.keeporiginal == "F":
+                        shutil.copy('genomes/' + current_name, 'renamed/' + new_name)
+                    else:
+                        os.rename('genomes/' + current_name, 'renamed/' + new_name)
+                        
+                        
+            if len(problemfiles) > 0:
+                with open("rename_check.txt", 'w') as f:
+                    for item in problemfiles:
+                        f.writelines(item+'\n')
+                        
+                    
+
+                    
